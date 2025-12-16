73ba28490820a862e45bf51ba3f22840c35babaa
MatheusDelduque <matheusdelduque1@gmail.com>
2025-12-16 12:45:14 -0300

    refactor: alterado para convecao de nomenclatura

--- Services/PersonService.cs
+++ Services/PersonService.cs
@@ -8,14 +8,14 @@ public class PersonService : IPersonService
 {
     public void AddPerson(PersonEntity person)
     {
-        people.Add(person);
+        _people.Add(person);
     }
-    private readonly List<PersonEntity> people = [];
+    private readonly List<PersonEntity> _people = [];
 
     public List<PeopleResponse> FindAll()
     {
 
-        return [.. people.Select(pessoa => new PeopleResponse()
+        return [.. _people.Select(pessoa => new PeopleResponse()
         {
             Id = pessoa.Id.ToString(),
             Email = pessoa.Email
@@ -24,17 +24,17 @@ public class PersonService : IPersonService
 
     public PersonEntity? FindById(string id)
     {
-        return people.FirstOrDefault(pessoa => pessoa.Id.ToString() == id);
+        return _people.FirstOrDefault(pessoa => pessoa.Id.ToString() == id);
     }
 
     public PersonEntity? FindByEmail(string email)
     {
-        return people.FirstOrDefault(pessoa => pessoa.Email == email);
+        return _people.FirstOrDefault(pessoa => pessoa.Email == email);
     }
 
     public PersonEntity? FindByToken(string token)
     {
-        return people.FirstOrDefault(pessoa => pessoa.Account.Token == token);
+        return _people.FirstOrDefault(pessoa => pessoa.Account.Token == token);
     }
 
 }
